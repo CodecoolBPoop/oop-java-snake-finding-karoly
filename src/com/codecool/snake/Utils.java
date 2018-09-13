2d2e0be176a8411fcc884f0ba44b6457ca2c73bd
MattHowdy <mate.hodi9@gmail.com>
2018-09-13 19:33:30 +0200

    Game StartUp Menu, End menu + buttons added + design elements

--- src/com/codecool/snake/Utils.java
+++ src/com/codecool/snake/Utils.java
@@ -17,6 +17,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.application.Platform;
 import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 
 public class Utils {
@@ -42,46 +46,126 @@ public class Utils {
     }
 
     // display a restart button and restart the application
-    public static void includeRestartBtn(Game game) {
+//    public static void includeRestartBtn(Game game) {
+//        currentGame = game;
+//        Button button1 = new Button("Restart");
+//        button1.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent e) {
+//                System.out.println("The application restarts");
+//                currentGame.restart();
+//
+//            }
+//        });
+//        game.getChildren().add(button1);
+//        game.setBackground(Globals.backgr);
+//    }
+
+
+
+
+    public static void includeMenuButtons(Game game) {
         currentGame = game;
-        Button button1 = new Button("Restart");
-        button1.setOnAction(new EventHandler<ActionEvent>() {
+        Button start = new Button("Start");
+        buttonStyle(start, 490,300);
+
+        Button quit = new Button("Quit");
+        buttonStyle(quit, 490,380);
+
+        start.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                System.out.println("The application restarts");
-                currentGame.restart();
-
+                System.out.println("The game starts");
+                game.getChildren().remove(start);
+                game.getChildren().remove(quit);
+                game.setBackground(Globals.backgr);
+                currentGame.start();
             }
         });
-        game.getChildren().add(button1);
-        game.setBackground(Globals.backgr);
+
+        quit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("The game quited");
+                Platform.exit();
+                }
+            }
+        );
+        quitebuttonEvent(quit);
+        game.getChildren().add(start);
+        game.getChildren().add(quit);
     }
 
-
-    // display popup and end game
-    public static void popUpShowWhenGameOver() {
-        Platform.runLater(() -> {
-            Alert alert = new Alert(AlertType.NONE, "BUSTED! Would you like to restart the game? ", ButtonType.YES, ButtonType.NO);
-
-            System.out.println("Pop up alert");
-
-            alert.showAndWait();
-
-            if (alert.getResult() == ButtonType.YES) {
-                System.out.println("Restarting");
-                currentGame.restart();
-
-            } else if (alert.getResult() == ButtonType.NO) {
-                System.out.println("Exit game");
+    private static void quitebuttonEvent(Button button){
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("The game quited");
                 Platform.exit();
             }
         });
     }
-
-    public static void startPopup() {
-        Alert alert = new Alert(AlertType.NONE, "Press ENTER to start the game", ButtonType.OK);
-        alert.showAndWait();
+    private static void buttonStyle(Button button,int x, int y){
+        button.setLayoutX(x);
+        button.setLayoutY(y);
+        button.setMinSize(150, 50);
+        button.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
     }
+
+    // display popup and end game
+//    public static void popUpShowWhenGameOver() {
+//        Platform.runLater(() -> {
+//            Alert alert = new Alert(AlertType.NONE, "BUSTED! Would you like to restart the game? ", ButtonType.YES, ButtonType.NO);
+//
+//            System.out.println("Pop up alert");
+//
+//            alert.showAndWait();
+//
+//            if (alert.getResult() == ButtonType.YES) {
+//                System.out.println("Restarting");
+//                currentGame.restart();
+//
+//            } else if (alert.getResult() == ButtonType.NO) {
+//                System.out.println("Exit game");
+//                Platform.exit();
+//            }
+//        });
+//    }
+
+    public static void gameOver(){
+        currentGame.setBackground(Globals.bustedBackgr);
+        includeRestartButtons(currentGame);
+    }
+
+
+    public static void includeRestartButtons(Game game) {
+        Button restart = new Button("Restart");
+        buttonStyle(restart, 490,300);
+
+        Button quit = new Button("Quit");
+        buttonStyle(quit, 490,380);
+
+        restart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("The application restarts");
+                game.getChildren().remove(restart);
+                game.getChildren().remove(quit);
+                game.setBackground(Globals.backgr);
+                currentGame.restart();
+            }
+        });
+
+        quitebuttonEvent(quit);
+
+        game.getChildren().add(restart);
+        game.getChildren().add(quit);
+    }
+//
+//    public static void startPopup() {
+//        Alert alert = new Alert(AlertType.NONE, "Press ENTER to start the game", ButtonType.OK);
+//        alert.showAndWait();
+//    }
 
 }
 
